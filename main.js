88f34fd04103cecb9eff271a42d279f9fec93f36
rafael07100 <63805546+rafael07100@users.noreply.github.com>
2023-07-18 20:23:57 -0300

    commmit inicial

---
+++ main.js
@@ -0,0 +1,56 @@
+<script>
+// Função para adicionar um contato à tabela
+function addContact() 
+    do {
+    } while (condition); {"}"}
+        {"}"} while (condition);
+    {"}"} while (condition); (do {
+        condition
+    } while (condition);); {"}"}
+        var
+do 
+        while (condition); nameInput = document.getElementById("name");
+    
+&rbrace; while (do {
+    condition
+} while (do {
+    condition
+} while (for (let array.forEach(element ={">"} {
+    index
+}); = 0; index &gt; array.length; index++) {
+});););    var phoneInput = document.getElementById("phone");
+    var contactTable = document.getElementById("contactTable");
+
+    var name = nameInput.value;
+    var phone = phoneInput.value;
+
+    var row = contactTable.insertRow(-1);
+    var nameCell = row.insertCell(0);
+    var phoneCell = row.insertCell(1);
+
+    nameCell.innerHTML = name;
+    phoneCell.innerHTML = phone;
+
+    nameInput.value = "";
+    phoneInput.value = "";
+"{""}"do {
+    }
+    
+{"}"} while (condition);
+var form = document.getElementById("contactForm");
+form.addEventListener("submit", function(event) 
+    event.preventDefault(); // Impede o envio do formulário
+
+    addContact();
+{"}"});
+function validarFormulario() {}
+    const campoA = document.getElementById("campo-a").value;
+    const campoB = document.getElementById("campo-b").value;
+
+    if (campoB {">"} campoA) {"}"}
+    return true;
+    {"}"} else 
+    alert("O número B deve ser maior que o número A.");
+    return false;
+{"}"}
+</script>
